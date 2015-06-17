22aea4e37f6c7c2f1b8466f45a88b4905b806e0f
Andrew Chun <achun@box.com>
2015-06-16 18:40:54 -0700

    Added documentation for BOXMetadataTask

--- BoxContentSDK/BoxContentSDK/Models/BOXMetadataTask.h
+++ BoxContentSDK/BoxContentSDK/Models/BOXMetadataTask.h
@@ -8,12 +8,40 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ * BOXMetadataTask instances are used for creating metadata key/value pairs.
+ */
 @interface BOXMetadataTask : NSObject
 
+/**
+ * The path (aka. key) for a metadata attribute.
+ */
 @property (nonatomic, readwrite, strong) NSString *path;
+
+/**
+ * The value for a metadata attribute.
+ */
 @property (nonatomic, readwrite, strong) NSString *value;
 
+/**
+ * Initializes a BOXMetadataTask with a given operation to apply to a key/value pair.
+ *
+ * @param path The path (aka. key)
+ * @param value The value for the path.
+ *
+ * @return A BOXMetadataTask with a key/value pair.
+ */
 - (instancetype)initWithPath:(NSString *)path value:(NSString *)value;
+
+/**
+ * Initializes a BOXMetadataTask with a given operation to apply to a key/value pair.
+ *
+ * @param path The path (aka. key).
+ * @param value The value for the path.
+ * @param validate Checks for invalid parameter values.
+ *
+ * @return A BOXMetadataTask with a key/value pair.
+ */
 - (instancetype)initWithPath:(NSString *)path value:(NSString *)value validation:(BOOL)validate;
 
 @end
