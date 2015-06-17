9d9ce7b756f4de250c81fc41ea260e2c1a67086f
Andrew Chun <achun@box.com>
2015-06-16 18:45:19 -0700

    Added documentation for BOXMetadataTemplate

--- BoxContentSDK/BoxContentSDK/Models/BOXMetadataTemplate.h
+++ BoxContentSDK/BoxContentSDK/Models/BOXMetadataTemplate.h
@@ -8,10 +8,25 @@
 
 #import <BoxContentSDK/BoxContentSDK.h>
 
+/**
+ * Represents metadata template information. Aka, The schema for a template.
+ */
 @interface BOXMetadataTemplate : BOXModel
 
+/**
+ * The scope the template belongs to.
+ */
 @property (nonatomic, readwrite, strong) NSString *scope;
+
+/**
+ * The name of the template.
+ */
 @property (nonatomic, readwrite, strong) NSString *displayName;
+
+/**
+ * The custom fields available in the template. Stored as key/value pairs
+ * such that the values can either be a single option or many options in a list.
+ */
 @property (nonatomic, readwrite, strong) NSArray *fields;
 
 @end
